8d5cdd9600f009f5d0f5848a928c89b46f9f28a4
betsy <betsykawira52@gmail.com>
2025-10-01 23:18:14 +0300

    Add files via upload

---
+++ stella.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+    cout << "What is your name?\n";
+    string name;
+    cin >> name;
+    cout << "Your name is " << name<< ",\n";
+    system("pause");
+    return 0;
+}
